dc3a0c1ea4f03b0e42bff9927cb9b28e7869140e
MAZaved <saad.25018@gmail.com>
2025-05-04 21:30:07 +0600

    with logout automation

---
+++ cypress/e2e/secondTest.spec.js
@@ -0,0 +1,14 @@
+describe( 'Test logout', () => {
+
+    beforeEach('login to application',() => {
+        cy.loginToApplication()
+    })
+    
+    it('verify user can log out successfully', () => {
+        cy.wait(5000)
+        cy.contains('Settings').click()
+        cy.wait(3000)
+        cy.contains('Or click here to logout.').click()
+        cy.get('.navbar-nav').should('contain', 'Sign in')
+    })
+})
