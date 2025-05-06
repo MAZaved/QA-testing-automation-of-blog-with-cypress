0eb98c56664bce1cdd5f482b0bba68ffbc2da35e
MAZaved <saad.25018@gmail.com>
2025-05-06 23:42:21 +0600

    with cypress custom config with multiple reporters

--- cypress.config.js
+++ cypress.config.js
@@ -10,16 +10,21 @@ module.exports = defineConfig({
     apiUrl: "https://conduit-api.bondaracademy.com",
   },
 
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       const username = process.env.DB_USERNAME
       const password = process.env.DB_PASSWORD
 
-      if(!password)
-      {
-        throw new Error("missing PASSWORD environment variable")
-      }
+      // if(!password)
+      // {
+      //   throw new Error("missing PASSWORD environment variable")
+      // }
 
       config.env = {username, password}
       return config
--- cypress/e2e/secondTest.spec.js
+++ cypress/e2e/secondTest.spec.js
@@ -4,10 +4,10 @@ describe( 'Test logout', () => {
         cy.loginToApplication()
     })
     
-    it('verify user can log out successfully', () => {
-        cy.wait(5000)
+    it('verify user can log out successfully', {retries: 2} , () => {
+        // cy.wait(5000)
         cy.contains('Settings').click()
-        cy.wait(3000)
+        // cy.wait(3000)
         cy.contains('Or click here to logout.').click()
         cy.get('.navbar-nav').should('contain', 'Sign in')
     })
