6081be988bba1fbae4c89a0c17015329ff5ba4c6
MAZaved <saad.25018@gmail.com>
2025-05-12 23:42:50 +0600

    updated assertions

--- cypress/examples/assertions.spec.js
+++ cypress/examples/assertions.spec.js
@@ -32,4 +32,48 @@ context('Assertions', () => {
               .and('include', 'cypress.io')
         })
     })
+
+    describe('Explicit Assertions', () => {
+      // https://on.cypress.io/assertions
+      it('expect - make an assertion about a specified subject', () => {
+        // We can use Chai's BDD style assertions
+        expect(true).to.be.true
+        const o = { foo: 'bar' }
+
+        expect(o).to.equal(o)
+        expect(o).to.deep.equal({ foo: 'bar' })
+        // matching text using regular expression
+        expect('FooBar').to.match(/bar$/i)
+      })
+
+      it('pass your own callback function to should()', () => {
+        // Pass a function to should that can have any number
+        // of explicit assertions within it.
+        // The ".should(cb)" function will be retried
+        // automatically until it passes all your explicit assertions or times out.
+        cy.get('.assertions-p')
+          .find('p')
+          .should(($p) => {
+            // https://on.cypress.io/$
+            // return an array of texts from all of the p's
+            // @ts-ignore TS6133 unused variable
+            const texts = $p.map((i, el) => Cypress.$(el).text())
+
+            // jquery map returns jquery object
+            // and .get() convert this to simple array
+            const paragraphs = texts.get()
+
+            // array should have length of 3
+            expect(paragraphs, 'has 3 paragraphs').to.have.length(3)
+
+            // use second argument to expect(...) to provide clear
+            // message with each assertion
+            expect(paragraphs, 'has expected text in each paragraph').to.deep.eq([
+              'Some text from first p',
+              'More text from second p',
+              'And even more text from third p',
+            ])
+          })
+      })
+    })
 })
