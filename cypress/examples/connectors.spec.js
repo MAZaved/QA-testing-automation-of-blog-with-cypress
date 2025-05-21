0649728b353eb406a2fb68f673fd9d762eb36bf7
MAZaved <saad.25018@gmail.com>
2025-05-21 21:58:01 +0600

    with actions and connectors updated

--- cypress/examples/connectors.spec.js
+++ cypress/examples/connectors.spec.js
@@ -41,5 +41,30 @@ context('Connectors', () => {
       expect(baz).to.eq('baz')
     })
   })
-  
+})
+
+describe('.then()', () => {
+  it('invokes a callback function with the current subject', () => {
+    // https://on.cypress.io/then
+    cy.get('.connectors-list > li')
+      .then(($lis) => {
+        expect($lis, '3 items').to.have.length(3)
+        expect($lis.eq(0), 'first item').to.contain('Walk the dog')
+        expect($lis.eq(1), 'second item').to.contain('Feed the cat')
+        expect($lis.eq(2), 'third item').to.contain('Write JavaScript')
+      })
+  })
+
+  it('yields the returned value to the next command', () => {
+    cy.wrap(1)
+      .then((num) => {
+        expect(num).to.equal(1)
+
+        return 2
+      })
+      .then((num) => {
+        expect(num).to.equal(2)
+      })
+  })
+
 })
