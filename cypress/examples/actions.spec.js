653886219940762ab11d1fa703726f2e9b5ef7e9
MAZaved <saad.25018@gmail.com>
2025-05-26 20:37:01 +0600

    with navigations

--- cypress/examples/actions.spec.js
+++ cypress/examples/actions.spec.js
@@ -41,4 +41,20 @@ context('Actions', () => {
       .prev().should('have.attr', 'style', 'color: red;')
   })
   
+
+  it('.clear() - clears an input or textarea element', () => {
+    // https://on.cypress.io/clear
+    cy.get('.action-clear').type('Clear this text')
+      .should('have.value', 'Clear this text')
+      .clear()
+      .should('have.value', '')
+  })
+
+  it('.submit() - submit a form', () => {
+    // https://on.cypress.io/submit
+    cy.get('.action-form')
+      .find('[type="text"]').type('HALFOFF')
+    cy.get('.action-form').submit()
+      .next().should('contain', 'Your form has been submitted!')
+  })
 })
---
+++ cypress/examples/navigations.spec.js
@@ -0,0 +1,16 @@
+context('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io')
+    cy.get('.navbar-nav').contains('Commands').click()
+    cy.get('.dropdown-menu').contains('Navigation').click()
+  })
+
+  it('cy.go() - go back or forward in the browser\'s history', () => {
+    // https://on.cypress.io/go
+
+    cy.location('pathname').should('include', 'navigation')
+
+    cy.go('back')
+    cy.location('pathname').should('not.include', 'navigation')
+  })
+})
