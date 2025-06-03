9f30173f40ceb06fd110a6e03ed70a1711f4ef24
MAZaved <saad.25018@gmail.com>
2025-06-03 19:28:28 +0600

    updated

--- cypress/examples/navigations.spec.js
+++ cypress/examples/navigations.spec.js
@@ -31,4 +31,23 @@ context('Navigation', () => {
     // reload the page without using the cache
     cy.reload(true)
   })
+
+  it('cy.visit() - visit a remote url', () => {
+    // https://on.cypress.io/visit
+
+    // Visit any sub-domain of your current domain
+
+    // Pass options to the visit
+    cy.visit('https://example.cypress.io/commands/navigation', {
+      timeout: 50000, // increase total time for the visit to resolve
+      onBeforeLoad (contentWindow) {
+        // contentWindow is the remote page's window object
+        expect(typeof contentWindow === 'object').to.be.true
+      },
+      onLoad (contentWindow) {
+        // contentWindow is the remote page's window object
+        expect(typeof contentWindow === 'object').to.be.true
+      },
+    })
+  })
 })
